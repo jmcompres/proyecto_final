462d5986da0513f73ba2fb03b9d6eb129d5f255a
jmcompres <JMCA0001@ce.pucmm.edu.do>
2024-10-31 14:05:59 -0400

    Update Arista.java

--- src/main/java/com/backend/elemental/Arista.java
+++ src/main/java/com/backend/elemental/Arista.java
@@ -1,37 +1,37 @@
 package com.backend.elemental;
 
 public class Arista {
-    private int tiempo;
-    private int distancia;
-    private int costo;
+    private float tiempo;
+    private float distancia;
+    private float costo;
 
-    public Arista(int tiempo, int distancia, int costo) {
+    public Arista(float tiempo, float distancia, float costo) {
         this.tiempo = tiempo;
         this.distancia = distancia;
         this.costo = costo;
     }
 
-    public int getTiempo() {
+    public float getTiempo() {
         return tiempo;
     }
 
-    public void setTiempo(int tiempo) {
+    public void setTiempo(float tiempo) {
         this.tiempo = tiempo;
     }
 
-    public int getDistancia() {
+    public float getDistancia() {
         return distancia;
     }
 
-    public void setDistancia(int distancia) {
+    public void setDistancia(float distancia) {
         this.distancia = distancia;
     }
 
-    public int getCosto() {
+    public float getCosto() {
         return costo;
     }
 
-    public void setCosto(int costo) {
+    public void setCosto(float costo) {
         this.costo = costo;
     }
 
